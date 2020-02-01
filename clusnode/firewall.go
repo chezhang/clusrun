60f8ce1ab414c09a6455f06457f4ca89334e8fe6
FAREAST\chezhang <chezhang@microsoft.com>
2020-02-02 02:32:28 +0800

    Fix a bug that firewall setup fails if executable path contains space

--- clusnode/firewall.go
+++ clusnode/firewall.go
@@ -15,8 +15,9 @@ func SetupFireWall() {
 			program := fmt.Sprintf("program=\"%v\"", executable_path)
 			bound_dir := fmt.Sprintf("dir=%v", dir)
 			cmd_del_fw := []string{"netsh", "advfirewall", "firewall", "delete", "rule", rule_name}
-			cmd_add_fw := []string{"netsh", "advfirewall", "firewall", "add", "rule", rule_name, program,
-				"protocol=tcp", bound_dir, "enable=yes", "action=allow", "profile=private,domain,public"}
+			cmd_add_fw := []string{"netsh", "advfirewall", "firewall", "add", "rule", rule_name, bound_dir,
+				"protocol=tcp", "enable=yes", "action=allow", "profile=private,domain,public"}
+			cmd_add_fw = append(cmd_add_fw, strings.Split(program, " ")...)
 			for _, cmd := range [][]string{cmd_del_fw, cmd_add_fw} {
 				log.Printf("Run command: %v", strings.Join(cmd, " "))
 				output, _ := exec.Command(cmd[0], cmd[1:]...).CombinedOutput()
