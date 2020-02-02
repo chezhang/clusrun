cf470a65e02245959deb564903409615c49e573c
FAREAST\chezhang <chezhang@microsoft.com>
2020-02-02 19:38:02 +0800

    Fix a bug that uninstallation fails if clusrun is installed in a path containing space

--- deployment/uninstall.bat
+++ deployment/uninstall.bat
@@ -1,9 +1,9 @@
 sc stop clusnode
 sc delete clusnode
-rmdir /Q /S %~dp0clusnode.exe.db
-rmdir /Q /S %~dp0clusnode.exe.log
-del %~dp0clusnode.exe.config
+rmdir /Q /S "%~dp0clusnode.exe.db"
+rmdir /Q /S "%~dp0clusnode.exe.log"
+del "%~dp0clusnode.exe.config"
 ping 127.0.0.1 -n 2 > nul
-del %~dp0clusnode.exe
-del %~dp0clus.exe
+del "%~dp0clusnode.exe"
+del "%~dp0clus.exe"
 ( del /q /f "%~f0" >nul 2>&1 & exit /b 0 )
