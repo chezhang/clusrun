dcc9c262af38879ef0e4069873c717058874adc1
FAREAST\chezhang <chezhang@microsoft.com>
2020-02-20 14:02:37 +0800

    Enable keeping data when uninstalling

--- deployment/uninstall.bat
+++ deployment/uninstall.bat
@@ -1,8 +1,12 @@
 sc stop clusnode
 sc delete clusnode
-rmdir /Q /S "%~dp0clusnode.exe.db"
-rmdir /Q /S "%~dp0clusnode.exe.log"
-del "%~dp0clusnode.exe.config"
+
+IF /I "%1"=="-cleanup" (
+    rmdir /Q /S "%~dp0clusnode.exe.db"
+    rmdir /Q /S "%~dp0clusnode.exe.log"
+    del "%~dp0clusnode.exe.config"
+)
+
 ping 127.0.0.1 -n 2 > nul
 del "%~dp0clusnode.exe"
 del "%~dp0clus.exe"
