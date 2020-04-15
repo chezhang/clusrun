8b3cfc4166383bf0584c727a049448cb6e636a81
FAREAST\chezhang <chezhang@microsoft.com>
2020-04-15 17:01:08 +0800

    set windows firewall to allow port instead of program

--- clusnode/firewall.go
+++ clusnode/firewall.go
@@ -8,20 +8,17 @@ import (
 
 func SetupFireWall() {
 	if RunOnWindows {
-		for _, dir := range []string{"in", "out"} {
-			LogInfo("Setup %v-bound firewall", dir)
-			rule_name := fmt.Sprintf("name=clusnode-%v", dir)
-			program := fmt.Sprintf("program=\"%v\"", ExecutablePath)
-			bound_dir := fmt.Sprintf("dir=%v", dir)
-			cmd_del_fw := []string{"netsh", "advfirewall", "firewall", "delete", "rule", rule_name}
-			cmd_add_fw := []string{"netsh", "advfirewall", "firewall", "add", "rule", rule_name, bound_dir,
-				"protocol=tcp", "enable=yes", "action=allow", "profile=private,domain,public"}
-			cmd_add_fw = append(cmd_add_fw, strings.Split(program, " ")...)
-			for _, cmd := range [][]string{cmd_del_fw, cmd_add_fw} {
-				LogInfo("Run command: %v", strings.Join(cmd, " "))
-				output, _ := exec.Command(cmd[0], cmd[1:]...).CombinedOutput()
-				LogInfo("Command output: %s", output)
-			}
+		_, port, _, _ := ParseHostAddress(NodeHost)
+		LogInfo("Setup in-bound firewall of port %v", port)
+		rule_name := fmt.Sprintf("name=clusnode-%v", port)
+		allow_port := fmt.Sprintf("localport=%v", port)
+		cmd_del_fw := []string{"netsh", "advfirewall", "firewall", "delete", "rule", rule_name}
+		cmd_add_fw := []string{"netsh", "advfirewall", "firewall", "add", "rule", rule_name, allow_port,
+			"dir=in", "protocol=tcp", "enable=yes", "action=allow", "profile=private,domain,public"}
+		for _, cmd := range [][]string{cmd_del_fw, cmd_add_fw} {
+			LogInfo("Run command: %v", strings.Join(cmd, " "))
+			output, _ := exec.Command(cmd[0], cmd[1:]...).CombinedOutput()
+			LogInfo("Command output: %s", output)
 		}
 	} else {
 		// TODO
