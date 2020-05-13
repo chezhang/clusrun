beabb2890bf59eaaaff8bf0ca0eede6b974eb39a
chezhang <chezhang@microsoft.com>
2020-05-13 19:27:33 +0800

    add firewall setup for Linux(rhel) nodes

--- clusnode/firewall.go
+++ clusnode/firewall.go
@@ -7,20 +7,23 @@ import (
 )
 
 func SetupFireWall() {
+	_, port, _, _ := ParseHostAddress(NodeHost)
+	LogInfo("Setup firewall of port %v", port)
+	var cmds [][]string
 	if RunOnWindows {
-		_, port, _, _ := ParseHostAddress(NodeHost)
-		LogInfo("Setup in-bound firewall of port %v", port)
 		rule_name := fmt.Sprintf("name=clusnode-%v", port)
-		allow_port := fmt.Sprintf("localport=%v", port)
 		cmd_del_fw := []string{"netsh", "advfirewall", "firewall", "delete", "rule", rule_name}
-		cmd_add_fw := []string{"netsh", "advfirewall", "firewall", "add", "rule", rule_name, allow_port,
+		cmd_add_fw := []string{"netsh", "advfirewall", "firewall", "add", "rule", rule_name, fmt.Sprintf("localport=%v", port),
 			"dir=in", "protocol=tcp", "enable=yes", "action=allow", "profile=private,domain,public"}
-		for _, cmd := range [][]string{cmd_del_fw, cmd_add_fw} {
-			LogInfo("Run command: %v", strings.Join(cmd, " "))
-			output, _ := exec.Command(cmd[0], cmd[1:]...).CombinedOutput()
-			LogInfo("Command output: %s", output)
-		}
+		cmds = [][]string{cmd_del_fw, cmd_add_fw}
 	} else {
-		// TODO
+		cmd_add_fw := []string{"firewall-cmd", "--permanent", fmt.Sprintf("--add-port=%v/tcp", port)}
+		cmd_reload := []string{"firewall-cmd", "--reload"}
+		cmds = [][]string{cmd_add_fw, cmd_reload}
+	}
+	for _, cmd := range cmds {
+		LogInfo("Run command: %v", strings.Join(cmd, " "))
+		output, _ := exec.Command(cmd[0], cmd[1:]...).CombinedOutput()
+		LogInfo("Command output: %s", output)
 	}
 }
